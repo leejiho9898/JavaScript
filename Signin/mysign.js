f3bb870d6efe724eda963a3b4e67c7aa90f67246
이지호 <cctv1998@naver.com>
2021-06-02 16:19:49 +0900

    finish

--- Signin/mysign.js
+++ Signin/mysign.js
@@ -1,50 +1,47 @@
+"use strict";
 
+function idcheck() {
+  var Id = document.getElementById("ID").value;
+  //아이디 조건
+  if (Id.length > 13) {
+    alert("아이디는 13자리 미만이어야 합니다.");
+    return false;
+  } else {
+    return true;
+  }
+}
+s;s
 
-    function idcheck(){//아이디 조건
-        if (Id.length > 13) {
-            alert('아이디는 13자리 미만이어야 합니다.')
-            return false;
-            }
-            else{
-                return true;
-            }
-        }
+function idcheck2() {
+  //아이디 중복체크
+  //백앤드 배우면 추가
+}
 
-    function idcheck2(){ //아이디 중복체크
-    //백앤드 배우면 추가
-    }
-    
-    function pwcheck() { //비밀번호 조건
-        var spe = /[`~!@#$%^&*()]/gi;
-        var alp = /[a-zA-Z]/;
-        var num = /[1-9]/;
-        if (Pw1 != Pw2) {
-            alert("비밀번호가 일치하지 않습니다");
-            return false;
-        }
-        else if (pw1.length<6||pw1.length>16) {
-            alert("비밀번호는 6글자 이상, 16글자 이하만 이용가능합니다.")
-            return false;
-        }
-        else if(spe.test(Pw1)==false && alp.test(Pw1)==false && num.test(Pw1)==false){
-            alert("비밀번호는 반드시 특수문자, 알파벳, 숫자를 포함해야 합니다.")
-            return false;
-            }
-        else{
-            return true;
-            }
-        }
+function pwcheck() {
+  var Pw1 = document.getElementById("pw").value,
+    Pw2 = document.getElementById("pwi").value;
+  //비밀번호 조건
+  var spe = /[`~!@#$%^&*()]/gi;
+  var alp = /[a-zA-Z]/;
+  var num = /[0-9]/g;
+  if (Pw1 != Pw2) {
+    alert("비밀번호가 일치하지 않습니다");
+    return false;
+  } else if (Pw1.length < 6 || Pw1.length > 16) {
+    alert("비밀번호는 6글자 이상, 16글자 이하만 이용가능합니다.");
+    return false;
+  } else if (spe.test(Pw1) == false || alp.test(Pw1) == false || num.test(Pw1) == false) {
+    alert("비밀번호는 반드시 특수문자, 알파벳, 숫자를 포함해야 합니다.");
+    return false;
+  } else {
+    return true;
+  }
+}
 
-    function submithandle(){
-        var Id=document.getElementById('ID').value,
-        Pw1=document.getElementById('pw').value,
-        Pw2=document.getElementById('pwi').value,
-        Nick = document.getElementById('nickname').value,
-        Phone = document.getElementById('phonenumber').value;
-        if (idcheck(Id)==true && pwcheck(Pw1,Pw2) == true){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+function submithandle() {
+  if (idcheck() == true && pwcheck() == true) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- onoff/onoff.js
+++ onoff/onoff.js
@@ -1,7 +1,48 @@
-function time(){
-    var time= new Date();
-    var hour= time.getHours,
-    var minute= time.getMinutes,
-    var second= time.getSeconds;
-       // 초까지 받음
-    }
+
+function getTime(){
+   let time= new Date();
+   let hour= time.getHours(), 
+   minute= time.getMinutes(), 
+   second= time.getSeconds(),
+   year=time.getFullYear(),
+   month=time.getMonth(),
+   date=time.getDate();
+
+   let todayis = year+"년 "+month+"월 "+date + "일 " + hour+"시 "+ minute+"분 "+second +"초";
+
+   document.querySelector("#nowday").innerText=(`${todayis}`);
+}
+
+function flow(){
+   getTime();
+   setInterval(getTime,1000);
+}
+flow();
+
+let takeIime=document.querySelector("#check-time"),
+    takeImg=document.querySelector("#onoff-img"),
+    takeOnoff=document.querySelector("#isonoff");
+
+
+function changeOn(){
+   let time= new Date();
+   let hour= time.getHours(), 
+   minute= time.getMinutes(), 
+   second= time.getSeconds();
+   let ontime ="체크시간 : " + hour+"시 "+ minute+"분 "+second +"초";
+
+   takeIime.innerText=(`${ontime}`);
+   takeImg.style.backgroundImage="url(work.jpg)";
+   takeOnoff.innerText=("근무중");
+}
+function changeOff(){
+   let time= new Date();
+   let hour= time.getHours(), 
+   minute= time.getMinutes(), 
+   second= time.getSeconds();
+   let ontime ="체크시간 : " + hour+"시 "+ minute+"분 "+second +"초";
+
+   takeIime.innerText=(`${ontime}`);
+   takeImg.style.backgroundImage="url(rest.jpg)";
+   takeOnoff.innerText=("휴식중");
+}
